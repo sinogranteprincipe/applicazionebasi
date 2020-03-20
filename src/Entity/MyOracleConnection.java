017cf1ce7954f51a4ea64cefb7cde699aed4f236
Aldo Di Giovanni <50913753+aldodg@users.noreply.github.com>
2020-03-20 11:50:42 +0100

    Revert "monnezza"
    This reverts commit c18b3b6be06eeeec606d3f1eb6fceca6a3ecd45a.

--- src/Entity/MyOracleConnection.java
+++ src/Entity/MyOracleConnection.java
@@ -7,18 +7,9 @@ import java.sql.SQLException;
 public class MyOracleConnection {
     private static MyOracleConnection instance;
     private Connection connection;
-    /*private String url ="jdbc:oracle:thin:@localhost:1521:XE";
+    private String url ="jdbc:oracle:thin:@localhost:1521:XE";
     private String username ="sino";
     private String password = "progetto";
-    */
-    private String url ="jdbc:oracle:thin:@192.168.1.13:1521:XE";
-    private String username ="system";
-    private String password = "accro";
-
-    private String host;
-    private String port;
-    private String database;
-    //private String url = "jdbc:oracle:thin:@"+host+":"+"port"+":"+database;
 
     private MyOracleConnection()throws SQLException{
         try{
@@ -29,31 +20,10 @@ public class MyOracleConnection {
         }
     }
 
-
     public Connection getConnection(){
         return connection;
     }
 
-/*
-    private MyOracleConnection(String hostTextField, String usernameTextField, String passwordTextField, String portaTextField, String databaseTextField)throws SQLException{
-        try{
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-            //this.connection = DriverManager.getConnection(hostTextField, usernameTextField, passwordTextField, portaTextField, databaseTextField);
-            //this.connection = DriverManager.getConnection(host username, password, port, database);
-        }catch(Exception ex){
-            System.out.println("Database Connection Creation Failed: " + ex.getMessage());
-        }
-    }
-
-    public Connection getConnection(String hostTextField, String usernameTextField, String passwordTextField, String portaTextField, String databaseTextField) {
-        this.username = usernameTextField;
-        this.password = passwordTextField;
-        this.host = hostTextField;
-        this.port = portaTextField;
-        this.database = databaseTextField;
-        return connection;
-    }
-*/
     public static MyOracleConnection getInstance() throws SQLException{
         if(instance == null){
             instance = new MyOracleConnection();
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import Entity.ClassDiagram.ClassDiagram;
 import Entity.ClassDiagram.ClassDiagramDAO;
 import View.Components.FrameSetter;
 import View.Components.MenuBarBuilder;
-import View.Login;
 import com.formdev.flatlaf.FlatDarculaLaf;
 
 import javax.swing.*;
@@ -10,7 +9,6 @@ import java.awt.*;
 import java.sql.SQLException;
 
 public class Main {
-
     public static void main(String[] args) {
         LookAndFeel lookAndFeel = new FlatDarculaLaf();
         try {
@@ -22,10 +20,8 @@ public class Main {
         MenuBarBuilder menuBarBuilder = new MenuBarBuilder();
         JMenuBar mb = MenuBarBuilder.getMenuBar();
         menuBarBuilder.setHomePageMenuBar();
-        //f.getContentPane().add(new JLabel("<html>Benvenuto!<br/>L'applicazione consente la gestione di un database per la memorizzazione di class diagram.</html>", SwingConstants.CENTER));
+        f.getContentPane().add(new JLabel("<html>Benvenuto!<br/>L'applicazione consente la gestione di un database per la memorizzazione di class diagram.</html>", SwingConstants.CENTER));
         f.setJMenuBar(mb);
         f.setVisible(true);
-        f.setContentPane(new Login().getView());
-        f.validate();
     }
-}
+   }
